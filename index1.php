99cb04d5b38448f23b8cea70dc49864f2314a0b3
吉田綾乃 <ayano@YayaMac.local>
2019-08-18 18:11:02 +0900

    fetch data and display with message

--- index1.php
+++ index1.php
@@ -8,5 +8,11 @@
 
     $query = "SELECT * FROM users";
     //ノート参照
-/*ここまで118*/
+    if($result = mysqli_query($link, $query)){
+        echo "クエリは正しく発行されました。";   
+    }
+    $row = mysqli_fetch_array($result);
+    echo "<p>あなたのメールアドレスは".$row['email'].",パスワードは".$row['password']."です.</p>";
+    //print_r($row);
+    //print_rして配列表示した場合はソースから見るとより見やすい。
 ?>
