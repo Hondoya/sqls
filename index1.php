ed6a36e3ef8e5361ce2ae019f828bac4dc70c0b8
吉田綾乃 <ayano@YayaMac.local>
2019-08-18 17:24:03 +0900

    second time

--- index1.php
+++ index1.php
@@ -1,13 +1,12 @@
 <?php
-    echo "Hello, world!";
- $link = mysqli_connect("localhost","root","root","memberapp");
+    $link = mysqli_connect("localhost","root","root","memberapp");
         //サーバー名、ユーザ名、パスワード、データベース名
 
     if(mysqli_connect_error()){
-        echo "データベースへのアクセスに失敗しました";
-    } else {
-        echo "アクセスは正常です";
+        die( "データベースへのアクセスに失敗しました");
     }
 
+    $query = "SELECT * FROM users";
+    //ノート参照
 /*ここまで118*/
 ?>
