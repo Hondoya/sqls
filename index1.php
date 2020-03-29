e1e8cf9de3e015ff0a20ebe37a450d1fc8b79b14
吉田綾乃 <ayano@YayaMac.local>
2020-03-29 13:25:50 +0900

    Lecture 125, Get multiple data

--- index1.php
+++ index1.php
@@ -5,31 +5,19 @@
     if(mysqli_connect_error()){
         die( "データベースへのアクセスに失敗しました");
     }
-    /*$query = "INSERT INTO `users` (`email`,`password`,`name`) VALUES ('kennethsjoe@gmail.com','chotcho','Kenneth Sjoebrom')";
 
-    if($result = mysqli_query($link, $query)){
-        echo "INSERTクエリは正しく発行されました。";   
-    
-    $query = "UPDATE `users` SET password = 'astra-modified' WHERE email = 'astra@mail.com' LIMIT 1";}
-//ここまで123
-    
-    if($result = mysqli_query($link, $query)){
-        echo "UPDATEクエリは正しく発行されました。"; 
-        echo "<br>";
-    }}*/
-
-    $query = "SELECT * FROM users";
-    //ノート参照
+    $query = "SELECT * FROM users WHERE email LIKE '%gmail.com'";
 
     if($result = mysqli_query($link,$query)){
-        echo "クエリの取得に成功しました";
+        echo "クエリの発行に成功しました";
     }
-    $row = mysqli_fetch_array($result);
-
-    //print_r($row);
-
+    while($row = mysqli_fetch_array($result)){
+        print_r($row);
+    }
+//ここまでLec125 where clause と while による複数データの取り出し
+ /*
     echo "<p>あなたのメールアドレスは ".$row['email'].", パスワードは".$row['password']." です.</p>";
-
+*/
 
 
 ?>
