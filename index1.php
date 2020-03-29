61e00369637caa96e91bbe52f3eb3ceec923f564
吉田綾乃 <ayano@YayaMac.local>
2020-03-29 10:51:10 +0900

    Lecture 123 UPDATE query

--- index1.php
+++ index1.php
@@ -5,14 +5,31 @@
     if(mysqli_connect_error()){
         die( "データベースへのアクセスに失敗しました");
     }
+    /*$query = "INSERT INTO `users` (`email`,`password`,`name`) VALUES ('kennethsjoe@gmail.com','chotcho','Kenneth Sjoebrom')";
+
+    if($result = mysqli_query($link, $query)){
+        echo "INSERTクエリは正しく発行されました。";   
+    
+    $query = "UPDATE `users` SET password = 'astra-modified' WHERE email = 'astra@mail.com' LIMIT 1";}
+//ここまで123
+    
+    if($result = mysqli_query($link, $query)){
+        echo "UPDATEクエリは正しく発行されました。"; 
+        echo "<br>";
+    }}*/
 
     $query = "SELECT * FROM users";
     //ノート参照
-    if($result = mysqli_query($link, $query)){
-        echo "クエリは正しく発行されました。";   
+
+    if($result = mysqli_query($link,$query)){
+        echo "クエリの取得に成功しました";
     }
     $row = mysqli_fetch_array($result);
-    echo "<p>あなたのメールアドレスは".$row['email'].",パスワードは".$row['password']."です.</p>";
+
     //print_r($row);
-    //print_rして配列表示した場合はソースから見るとより見やすい。
+
+    echo "<p>あなたのメールアドレスは ".$row['email'].", パスワードは".$row['password']." です.</p>";
+
+
+
 ?>
